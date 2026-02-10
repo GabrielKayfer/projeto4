6004745fd490f807b4c900256abcd061aeae3961
Gabriel Kayfer <gabrielkayfer@icloud.com>
2026-02-10 16:11:30 -0300

    fix error

--- gulpfile.js
+++ gulpfile.js
@@ -4,31 +4,41 @@ const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 
 function scripts() {
-    return gulp.src('./src/scripts/*.js')
+  return gulp.src('./src/scripts/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('./dist/js'));
 }
 
 function styles() {
-    return gulp.src('./src/styles/*.scss')
-    .pipe(sass({ outputStyle: 'compressed'}))
+  return gulp.src('./src/styles/main.scss')
+    .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(gulp.dest('./dist/css'));
 }
 
 function images() {
-    return gulp.src('./src/images/**/*')
+  return gulp.src('./src/images/**/*')
     .pipe(imagemin())
     .pipe(gulp.dest('./dist/images'));
 }
 
+// copia o index.html da raiz para o dist
 function html() {
-    return gulp.src('./src/*.html')
-        .pipe(gulp.dest('./dist'));
+  return gulp.src('./index.html')
+    .pipe(gulp.dest('./dist'));
 }
 
-exports.default = gulp.parallel(styles, images, scripts, html);
+// copia fontes para dentro do dist
+function fonts() {
+  return gulp.src('./assents/fonts/**/*')
+    .pipe(gulp.dest('./dist/fonts'));
+}
 
-exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
-    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
+exports.default = gulp.parallel(styles, scripts, images, html, fonts);
+
+exports.watch = function () {
+  gulp.watch('./src/styles/**/*.scss', styles);
+  gulp.watch('./src/scripts/*.js', scripts);
+  gulp.watch('./src/images/**/*', images);
+  gulp.watch('./index.html', html);
+  gulp.watch('./assents/fonts/**/*', fonts);
 }
